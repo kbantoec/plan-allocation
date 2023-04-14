e29326b4cd6b77c0d20b3cc842800df3e1dedfc2
Kai Yannick Banto Eckert <y.banto@outlook.com>
2023-04-14 19:00:43 +0200

    Problem 3

--- src/workspace/PROBLEMS.md
+++ src/workspace/PROBLEMS.md
@@ -21,6 +21,21 @@ A company receives three project demands. The completion of each project require
 | Data science | 12 | 30 | 40 | 82 |
 | Total per project | 34 | 53 | 75 | 162 |
 
+This matrix is formed by a binary matrix and a quantities' matrix:
+```python
+skill_per_project = np.array([[1, 1, 1],
+                              [1, 0, 1],
+                              [1, 1, 1]])
+skill_per_project_plan = np.array([[18, 23, 15],
+                                    [4, 0, 20],
+                                    [12, 30, 100]])
+
+>>> skill_per_project * skill_per_project_plan
+array([[ 18,  23,  15],
+       [  4,   0,  20],
+       [ 12,  30, 100]])
+```
+
 Suppose that the timeframe is a quarter. A quarter has 60 business days (5 business days × 4 weeks per month × 3 months) of 8 hours.
 
 The activity rate, or the participation, of each resource pool is:
@@ -40,7 +55,56 @@ Their skills are:
 | Sponge Bob | 0 | 1 | 0 |
 | Cute Pandas | 0 | 0 | 1 |
 
+These tables above can be represented as matrices:
+```python
+import numpy as np
+import pandas as pd
+import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
+
+
+if __name__ == "__main__":
+    days_in_a_quarter: int = 60
+
+    skill_per_project = np.array([[1, 1, 1],
+                                 [1, 0, 1],
+                                 [1, 1, 1]])
+
+    skill_per_project_plan = np.array([[18, 23, 15],
+                                       [4, 0, 20],
+                                       [12, 30, 100]])
+
+    skill_per_person = np.array([[1, 0, 1],
+                                 [0, 1, 0],
+                                 [0, 0, 1]])
+
+    participation = np.array([1, 0.8, 1])
+
+    capacity_per_person = participation * days_in_a_quarter
+    # array([60., 48., 60.])
+
+    capacity_per_skill = np.sum(capacity_per_person * skill_per_person, axis=0)
+    # array([ 60.,  48., 120.])
+```
+
 Project 1 has priority 3 (highest), project 2 has priority 1, and project 3 has priority 2.
+
+## Sets
+
+```python
+# Model instantiation
+model_1 = pyo.ConcreteModel()
+
+# Sets
+projects: list[str] = ["P1", "P2", "P3"]
+model_1.projects = pyo.Set(initialize=projects)
+
+skills: list[str] = ["project_management", "data_modeling", "data_science"]
+model_1.skills = pyo.Set(initialize=skills)
+
+collaborators: list[str] = ["John Doe", "Sponge Bob", "Cute Pandas"]
+model_1.collaborators = pyo.Set(initialize=collaborators)
+```
 
 What is the best resource allocation?
 
@@ -58,7 +122,50 @@ $$\max 3x_1 + 1x_2 + 2x_3$$
 $$s.t.$$
 $$x_i={0, 1}$$
 
-Moreover, we cannot exceed 60HD of project management, 48HD of data modeling, and 120HD of data science. 
+
+## Constraints
+
+### The human capacity or capacity per collaborator
+When performing the allocation plan, we have to bear in mind that a collaborator cannot exceed their total capacity, $\kappa$. The total capacity for each collaborator is:
+
+| Ressource | Participation | Availability in days |
+| - | - | - |
+| John Doe | 100% | 60 |
+| Sponge Bob | 80% | 48 |
+| Cute Pandas | 100% | 60 |
+
+And can be represented as a vector $h=[60, 48, 60]$.
+
+The constraints therefore are:
+$$\kappa \cdot x \leq h$$
+Where,
+* $\kappa$ is the human days allocated to each project, in our case it is a $3 \times 3$ matrix;
+* $x$ is the projects' vector of $3 \times 1$.
+
+<!-- We also notice that $\kappa$ is a **decision variable** (i.e., optimisation vairable). It is what we want to calculate. -->
+
+### Total allocable HD per skill
+Moreover, we cannot exceed 60HD of project management, 48HD of data modeling, and 120HD of data science. This information is known beforehand. This is why can formulate parameters:
+```python
+skill_per_project_plan_df = pd.DataFrame(data=skill_per_project_plan.T, columns=skills, index=projects)
+
+#     project_management  data_modeling  data_science
+# P1                  18              4            12
+# P2                  23              0            30
+# P3                  15             20           100
+
+skill_per_project_plan_dict = skill_per_project_plan_df.to_dict(orient="dict")
+
+# {'project_management': {'P1': 18, 'P2': 23, 'P3': 15}, 
+#  'data_modeling': {'P1': 4, 'P2': 0, 'P3': 20},
+#  'data_science': {'P1': 12, 'P2': 30, 'P3': 100}}
+
+model_1.project_management = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["project_management"])
+
+model_1.data_modeling = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["data_modeling"])
+
+model_1.data_science = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["data_science"])
+```
 
 ```python
 import numpy as np
@@ -148,6 +255,35 @@ data_modeling          24.0
 data_science          112.0
 ```
 
+Project's matrix
+Sponge Bob's matrix
+| Skill | Project 1 (P1) | Project 2 (P2) | Project 3 (P3)
+| - | - | - | - |
+| Project management | 1 | 1 | 1 | 
+| Data modeling  | 1 | 0 | 1 | 
+| Data science | 1 | 1 | 1 | 
+
+John Doe's matrix
+| Skill | Project 1 (P1) | Project 2 (P2) | Project 3 (P3)
+| - | - | - | - |
+| Project management | 1 | 1 | 1 | 
+| Data modeling  | 0 | 0 | 0 | 
+| Data science | 1 | 1 | 1 |
+
+Sponge Bob's matrix
+| Skill | Project 1 (P1) | Project 2 (P2) | Project 3 (P3)
+| - | - | - | - |
+| Project management | 0 | 0 | 0 | 
+| Data modeling  | 1 | 0 | 1 | 
+| Data science | 0 | 0 | 0 | 
+
+Cuta Pandas' matrix
+Sponge Bob's matrix
+| Skill | Project 1 (P1) | Project 2 (P2) | Project 3 (P3)
+| - | - | - | - |
+| Project management | 0 | 0 | 0 | 
+| Data modeling  | 0 | 0 | 0 | 
+| Data science | 1 | 1 | 1 |
 
 Can we map capacity to the persons?
 
--- src/workspace/problem_02.py
+++ src/workspace/problem_02.py
@@ -6,13 +6,21 @@ from pyomo.opt import SolverFactory
 
 if __name__ == "__main__":
     days_in_a_quarter: int = 60
-    skill_per_project = np.array([[18, 23, 15],
-                                  [4, 0, 20],
-                                  [12, 30, 100]])
+
+    skill_per_project = np.array([[1, 1, 1],
+                                 [1, 0, 1],
+                                 [1, 1, 1]])
+
+    skill_per_project_plan = np.array([[18, 23, 15],
+                                       [4, 0, 20],
+                                       [12, 30, 100]])
+
     skill_per_person = np.array([[1, 0, 1],
                                  [0, 1, 0],
                                  [0, 0, 1]])
+
     participation = np.array([1, 0.8, 1])
+
     capacity_per_person = participation * days_in_a_quarter  # array([60., 48., 60.])
     capacity_per_skill = np.sum(capacity_per_person * skill_per_person, axis=0)  # array([ 60.,  48., 120.])
     print(f"The total allocable project management number of days: {capacity_per_skill[0]}")
@@ -27,16 +35,40 @@ if __name__ == "__main__":
     # Sets
     projects: list[str] = ["P1", "P2", "P3"]
     projects_priority: list[int] = [3, 1, 2]
+    model_1.projects = pyo.Set(initialize=projects)
+
     skills: list[str] = ["project_management", "data_modeling", "data_science"]
-    model_1.projects = pyo.Set(initialize=["P1", "P2", "P3"])
+    model_1.skills = pyo.Set(initialize=skills)  # Added the 4/4/2023
+
+    collaborators: list[str] = ["John Doe", "Sponge Bob", "Cute Pandas"]
+    model_1.collaborators = pyo.Set(initialize=collaborators)
 
     # Parameters
-    skill_per_project_df = pd.DataFrame(data=skill_per_project.T, columns=skills, index=projects)
-    # skill_per_project_dict = skill_per_project_df.to_dict(orient="records")
-    skill_per_project_dict = skill_per_project_df.to_dict(orient="dict")
-    model_1.project_management = pyo.Param(model_1.projects, initialize=skill_per_project_dict["project_management"])
-    model_1.data_modeling = pyo.Param(model_1.projects, initialize=skill_per_project_dict["data_modeling"])
-    model_1.data_science = pyo.Param(model_1.projects, initialize=skill_per_project_dict["data_science"])
+    skill_per_project_plan_df = pd.DataFrame(data=skill_per_project_plan.T, columns=skills, index=projects)
+    skill_per_project_plan_dict = skill_per_project_plan_df.to_dict(orient="dict")
+    model_1.project_management = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["project_management"])
+    model_1.data_modeling = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["data_modeling"])
+    model_1.data_science = pyo.Param(model_1.projects, initialize=skill_per_project_plan_dict["data_science"])
+
+    skill_per_project_df = pd.DataFrame(data=skill_per_project, columns=skills, index=projects)
+    skill_per_person_df = pd.DataFrame(data=skill_per_person, columns=skills, index=collaborators)
+    collaborators_per_project_matrices = {}
+    for c in collaborators:
+        collab = skill_per_person_df.loc[c].to_numpy() * skill_per_project
+        collaborators_per_project_matrices[c] = pd.DataFrame(data=collab, columns=skills, index=projects).T
+
+    # todo: check
+    # skill_per_person_df = pd.DataFrame(data=skill_per_person.T, columns=skills, index=collaborators)
+    # skill_per_person_dict = skill_per_person_df.to_dict(orient="dict")
+    # model_1.john = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["project_management"])
+    # model_1.bob = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["data_modeling"])
+    # model_1.cute = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["data_science"])
+
+    # person_per_project_df = pd.DataFrame(data=skill_per_person, columns=projects, index=collaborators)
+    # person_per_project_dict = person_per_project_df.to_dict(orient="dict")
+    # model_1.john = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["project_management"])
+    # model_1.bob = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["data_modeling"])
+    # model_1.cute = pyo.Param(model_1.collaborators, initialize=skill_per_person_dict["data_science"])
 
     model_1.priority = pyo.Param(model_1.projects, initialize=dict(zip(projects, projects_priority)))
 
@@ -59,10 +91,12 @@ if __name__ == "__main__":
         return sum(model.data_modeling[p] * model.x[p] for p in model.projects) <= capacity_per_skill[1]
 
     def data_science_constraint_callback(model: pyo.Model, i):
+        # return sum(model.data_science[project] * model.x[project]) <= capacity_per_skill[2]
         return sum(model.data_science[p] * model.x[p] for p in model.projects) <= capacity_per_skill[2]
 
-    def john_doe_constraint_callback(model: pyo.Model, i):
-        return sum
+    # todo
+    def john_doe_constraint_callback(model: pyo.Model, collaborator):
+        return sum(model.john[c] for c in model.collaborators) <= capacity_per_person[0]
 
     def project_outcome_constraint_callback(model: pyo.Model, project):
         return model.x[project] <= 1
@@ -76,9 +110,15 @@ if __name__ == "__main__":
     # def p3_outcome_constraint_callback(model: pyo.Model, p):
     #     return model.x["P3"] <= 1
 
+    # Skill constraint
     model_1.project_management_constraint = pyo.Constraint(model_1.projects, rule=project_management_constraint_callback)
     model_1.data_modeling_constraint = pyo.Constraint(model_1.projects, rule=data_modeling_constraint_callback)
     model_1.data_science_constraint = pyo.Constraint(model_1.projects, rule=data_science_constraint_callback)
+
+    # Capacity per collaborator constraint
+    model_1.john_constraint = pyo.Constraint(model_1.projects, rule=john_doe_constraint_callback)
+
+    # Outcome constraint
     model_1.project_outcome_constraint = pyo.Constraint(model_1.projects, rule=project_outcome_constraint_callback)
     # model_1.p1_outcome_constraint = pyo.Constraint(model_1.projects, rule=p1_outcome_constraint_callback)
     # model_1.p2_outcome_constraint = pyo.Constraint(model_1.projects, rule=p2_outcome_constraint_callback)
@@ -97,7 +137,7 @@ if __name__ == "__main__":
     for project in model_1.projects:
         print(f"{project} = {model_1.x[project]()}")
 
-    allocation_per_project = skill_per_project * np.array(list(model_1.x.get_values().values()))
+    allocation_per_project = skill_per_project_plan * np.array(list(model_1.x.get_values().values()))
     allocation_per_project_df = pd.DataFrame(data=allocation_per_project, columns=projects, index=skills)
 
     print("The allocation would be:")
